3a1c08230baeda5724ae40b15d26935967407373
aseme <aseme@ASEM>
2023-06-30 05:15:58 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import ReactDOM  from 'react-dom/client'
-function Greating(){
-    return <h2>My First Component</h2>
+import ReactDOM from 'react-dom/client'
+function Greating() {
+  return (
+    <>
+      <section>
+        <h2>My First Component</h2>
+      </section>
+      <input type="text" name="" id="" />
+    </>
+  )
 }
 
+// function Greating() {
+//   return React.createElement('h2',{},'hello world !!')
+// }
 
-
-const root=ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(<Greating/>) 
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<Greating />)
