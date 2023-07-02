a78fdf53d1a210370831fe869013c124a5bc0fd9
aseme <aseme@ASEM>
2023-07-02 19:45:19 +0300

    enw

--- src/Testing.js
+++
@@ -1,6 +0,0 @@
-const Testing = () => {
-  return (
-    <div>Testing</div>
-  )
-}
-export default Testing
--- src/index.js
+++ src/index.js
@@ -1,19 +1,66 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-function Greating() {
+import './index.css'
+
+const books = [
+  {
+    author: 'Jordan Moore',
+    title: 'interesting Facts For Curious Minds',
+    img: './images/book.jpg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
+  },
+]
+
+const BookList = () => {
   return (
-    <>
-      <section>
-        <h2>My First Component</h2>
-      </section>
-      <input type="text" name="" id="" />
-    </>
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id}/>
+      })}
+    </section>
   )
 }
 
-// function Greating() {
-//   return React.createElement('h2',{},'hello world !!')
+// const Book = (props) => {
+//   const { img, title, author } = props.book
+//   return (
+//     <article className="book">
+//       <img src={img} />
+//       <h2> {title} </h2>
+//       <h4>{author.toUpperCase()}</h4>
+//     </article>
+//   )
 // }
 
+
+
+// const Book = ({book:{img, title,author}}) => {
+//   return (
+//     <article className="book">
+//       <img src={img} />
+//       <h2> {title} </h2>
+//       <h4>{author.toUpperCase()}</h4>
+//     </article>
+//   )
+// }
+ 
+const Book = (props) => {
+  console.log(props)
+  const { img, title, author } = props
+  return (
+    <article className="book">
+      <img src={img} />
+      <h2> {title} </h2>
+      <h4>{author.toUpperCase()}</h4>
+    </article>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Greating />)
+root.render(<BookList />)
